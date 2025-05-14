68f5546702086830dec65f1e6952b7e5a1ae13ac
eranga <erangamartyn@gmail.com>
2025-05-15 04:39:07 +0530

    add TaskList class with listTasks method

--- js/taskList.js
+++ js/taskList.js
@@ -1,7 +1,24 @@
 function TaskList() {
-    
+    this.tasks = [];
 }
 
-TaskList.prototype.addTask = function(){
-    
+// Method to add a task
+TaskList.prototype.addTask = function(name, dueDate){
+    const task = new Task(name, dueDate);
+    this.tasks.push(task);
+}
+
+// Method to remove a task
+TaskList.prototype.removeTask = function (taskName) {
+    this.tasks = this.tasks.filter(task => task.name !== taskName);
+}
+
+// Method to list all tasks
+TaskList.prototype.listTask = function(){
+    this.tasks.forEach(task => task.diplay());
+}
+
+// Method get total tasks
+TaskList.prototype.getTotalTask = function () {
+    return this.tasks.length;
 }
